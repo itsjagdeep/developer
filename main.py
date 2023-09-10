4dadcd855bb9925b8e17d02397f93c5c091b6a10
itsjagdeep <jagdeep1834@gmail.com>
2023-09-10 11:03:08 -0400

    s

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+import modal
 from smol_dev.prompts import plan, specify_file_paths, generate_code_sync
 from smol_dev.utils import generate_folder, write_file
 from smol_dev.main import main
